2cdb991ed8d1c24a80cd2fd32404504c258477ce
bbairdgen <bbairdgen@gmail.com>
2022-12-28 22:33:20 -0700

    pseudocode added and initial variables created

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var startBtn = document.querySelector('#start-btn')
+var quizboxEl = document.querySelector('#quizbox')
+var highscoreEl = document.querySelector('#highscores')
+var timerEl = document.querySelector('#timer')
+
+var timer;
+var timerCount;
+
+// Timer starts when start button is clicked. 
+
+// First question is presented when start button is clicked
+
+// Following question is presented when question is answered correctly or incorrectly
+
+// Timer decreases by 10 seconds when incorrect answer is selected.
+
+// quiz is over when timer reaches 0
+
+// quiz is over when all questions are answered
+
+// At end of quiz, score (time) is saved with initials
+
+// When 'View Highscores' is clicked, stored highscores are displayed
+
+// Short message is displayed when question is answered correctly or incorrectly
